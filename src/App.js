2a20e661bea9239cf1cf5f651ef82e31efe95c10
sjproctor <sjproctor950@gmail.com>
2021-07-26 14:12:15 -0700

    adds alert with index in app.js

--- src/App.js
+++ src/App.js
@@ -10,13 +10,23 @@ class App extends Component{
     }
   }
 
+  handleGamePlay = (index) => {
+    alert(index)
+  }
+
   render(){
     return(
       <>
         <h1>Treasure Hunt Game</h1>
         <div id="gameboard">
           {this.state.board.map((value, index) => {
-            return <Square value={value} key={index} />
+            return (
+              <Square
+                value={value}
+                key={index}
+                index={index} handleGamePlay={this.handleGamePlay}
+              />
+            )
           })}
         </div>
       </>
